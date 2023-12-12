1d6ea1ecc0eb63c097d32d36321c9bfbd5499bd5
Ahmed Esmat <essmat884@gmail.com>
2023-12-13 01:08:36 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+import cors from "cors";
+import express from "express";
+import dotenv from 'dotenv'
+
+import formTemperatureAlertMail from "./services/mail.service.js";
+
+dotenv.config()
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+app.use(cors());
+
+app.post("/send-temperature-pressure", async (req, res) => {
+  console.log("request recived");
+  console.log(req.body);
+  const { temperature, email, threshold, pressure } = req.body;
+
+  const newData = { temperature, pressure };
+
+  if (temperature >= threshold) {
+    await formTemperatureAlertMail(email, temperature);
+    return res.status(200).json({ message: "email sent" });
+  }
+
+  res.status(200).json({ message: "success" });
+});
+
+app.get("/", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data.json"));
+  console.log(data);
+  res.status(200).json(data);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Invalid Route" });
+});
+
+const port = parseInt(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`App Running on port ${port}`);
+});
---
+++ services/mail.service.js
@@ -0,0 +1,43 @@
+import fs from "fs";
+
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const emailTemplatePath = `./public/email-template.html`;
+
+const transporterConfig = {
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT),
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+};
+
+export const sendEmail = async (mailOptions) => {
+  // create transporter
+  const transporter = nodemailer.createTransport(transporterConfig);
+
+  // send email
+  await transporter.sendMail(mailOptions);
+};
+
+const formTemperatureAlertMail = async (email, temperature) => {
+  let html = fs.readFileSync(emailTemplatePath, "utf-8");
+  html = html.replace("[TEMPERATURE]", temperature);
+  html = html.replace("[DATE_TIME]", new Date().toString());
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    html,
+    subject: `Temperature Alert`,
+  };
+
+  await sendEmail(mailOptions);
+};
+
+export default formTemperatureAlertMail;
